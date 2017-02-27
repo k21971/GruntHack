5342c95f578ed0007804678c411fc1f267e8127c
Tangles <andyrthomson@gmail.com>
2017-02-27 18:56:14 +1100

    Change dumplog path for hardfought.org server

--- include/config.h
+++ include/config.h
@@ -432,7 +432,7 @@ typedef unsigned char	uchar;
 /*#define GOLDOBJ */	/* Gold is kept on obj chains - Helge Hafting */
 #define AUTOPICKUP_EXCEPTIONS /* exceptions to autopickup */
 #define DUMP_LOG        /* Dump game end information to a file */
-#define DUMP_FN "/dgldir/userdata/%N/%n/dumplog/%t.gh020.txt"
+#define DUMP_FN "/dgldir/userdata/%N/%n/gh020/dumplog/%t.gh020.txt"
 /* #define DUMP_FN "/tmp/%n.nh" */      /* Fixed dumpfile name, if you want
                                          * to prevent definition by users */
 #define DUMPMSGS 20     /* Number of latest messages in the dump file  */
