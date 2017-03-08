37fa795f63cca2c448aa6a658bc78c8bbc59b90f
k21971 <keith.simpson1971@gmail.com>
2017-03-08 02:05:14 -0500

    autopen take two...

--- src/hack.c
+++ src/hack.c
@@ -17,6 +17,8 @@ STATIC_DCL boolean FDECL(monstinroom, (struct permonst *,int));
 
 STATIC_DCL void FDECL(move_update, (BOOLEAN_P));
 
+static boolean door_opened;	/* set to true if door was opened during test_move */
+
 #define IS_SHOP(x)	(rooms[x].rtype >= SHOPBASE)
 
 #ifdef OVL2
@@ -628,7 +630,7 @@ int mode;
 #ifdef AUTO_OPEN
 		    else if (iflags.autoopen
 				&& !Confusion && !Stunned && !Fumbling) {
-			    flags.move = doopen_indir(x, y);
+		    door_opened = flags.move = doopen_indir(x, y);
 		    }
 #endif
 		    else if (x == ux || y == uy) {
@@ -1465,9 +1467,13 @@ domove()
 	}
 
 	if (!test_move(u.ux, u.uy, x-u.ux, y-u.uy, DO_MOVE)) {
-	    flags.move = 0;
-	    nomul(0);
-	    return;
+	   if (!door_opened) {
+		flags.move = 0;
+		nomul(0);
+	   } else {
+		door_opened = 0;
+	   }
+	   return;
 	}
 
 	/* Move ball and chain.  */
@@ -1479,7 +1485,7 @@ domove()
 	/* Check regions entering/leaving */
 	if (!in_out_region(x,y))
 	    return;
-	
+
 	if (!Blind) {
 		struct obj *otmp = level.objects[u.ux][u.uy];
 		for (; otmp; otmp = otmp->nexthere) {
