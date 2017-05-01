5beace7b091b878feab5ee0f9f824392332ddf64
k21971 <keith.simpson1971@gmail.com>
2017-05-01 01:09:49 -0400

    Display items in inventory list (UnNetHack)

--- win/curses/cursdial.c
+++ win/curses/cursdial.c
@@ -557,7 +557,7 @@ void curses_create_nhmenu(winid wid)
 
 /* Add a menu item to the given menu window */
 
-void curses_add_nhmenu_item(winid wid, const ANY_P *identifier,
+void curses_add_nhmenu_item(winid wid, int glyph, const ANY_P *identifier,
  CHAR_P accelerator, CHAR_P group_accel, int attr, const char *str,
  BOOLEAN_P presel)
 {
