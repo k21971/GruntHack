1edfbea13d85932adc293b61224e5c5deb12ebaf
k21971 <keith.simpson1971@gmail.com>
2017-05-27 23:27:01 -0400

    livelogging update (hit w/ weapon)

--- src/dothrow.c
+++ src/dothrow.c
@@ -1490,6 +1490,10 @@ register struct obj   *obj;
 	    }
 
 	    if (tmp >= rnd(20)) {
+		u.uconduct.weaphit++
+		#ifdef LIVELOG
+  	        livelog_write_string("hit with a wielded weapon for the first time");
+		#endif
 		if (hmon(mon,obj,1)) {	/* mon still alive */
 		    cutworm(mon, bhitpos.x, bhitpos.y, obj);
 		}
